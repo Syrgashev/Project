559e84eaf43e1489ae6b13e83861cbcf4d20be54
vladimir.syrgashev <vladimir.syrgashev@gmail.com>
2019-10-01 13:32:01 +0300

    first version

---
+++ Counting_letters.py
@@ -0,0 +1,13 @@
+line_voina = ''
+for line in open('voyna-i-mir.txt'):
+    line_voina += line
+print(len(line_voina))
+lst_dict = {}
+for x in line_voina:
+    lst_dict.setdefault(x, 0)
+    lst_dict[x] += 1
+
+print(lst_dict)
+
+s = sorted(list(lst_dict.items()))
+print(s)
